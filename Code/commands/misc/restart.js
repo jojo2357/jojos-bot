a5330d98146bd5d26da693174f01f41514f0d6fd
jojo2357 <66704796+jojo2357@users.noreply.github.com>
2020-12-14 13:53:01 -0800

    Merge restart + shutdown

--- Code/commands/misc/restart.js
+++ Code/commands/misc/restart.js
@@ -7,16 +7,18 @@ module.exports = {
         client = klient;
     },
 
-    commands: ['restart'], 
-    minArgs: 0,
-    maxArgs: 0,
+    commands: ['restart'],
+    maxArgs: 1,
     callback: async (message, arguments) => {
-        if (message.author.id == '524411594009083933' || message.author.id == '777008421940887583'){
-            client.user.setActivity('Restarting, please wait');
-            await message.channel.send('Hasta la pasta');
-            await client.user.setAFK(true);
-            process.exit(1);
-        }
-        await message.channel.send('In ur dreams sukers');
+        if (message.author.id == '524411594009083933' || message.author.id == '777008421940887583') {
+            if (!arguments[0] || os.platform().toString().toLowerCase().includes(arguments[0].toLowerCase())) {
+                client.user.setActivity('Restarting, please wait');
+                await message.channel.send('Hasta la pasta');
+                await client.user.setAFK(true);
+                process.exit(1);
+            } else
+                message.send("I am " + os.platform().toString() + "");
+        } else
+            await message.channel.send('In ur dreams sukers');
     }
 }
--- Code/commands/misc/shutdown.js
+++ Code/commands/misc/shutdown.js
@@ -1,10 +1,16 @@
+const os = require('os');
+
 module.exports = {
     commands: ['shutdown'],
-    maxArgs: 0,
-    callback: async (message) => {
+    maxArgs: 1,
+    callback: async (message, arguments) => {
         await message.channel.send('oh no!');
         if (message.author == '524411594009083933')
-            process.exit(0);
-        await message.channel.send('anyway');
+            if (!arguments[0] || os.platform().toString().toLowerCase().includes(arguments[0]))
+                process.exit(0);
+            else
+                message.send("I am " + os.platform().toString() + "");
+        else
+            await message.channel.send('anyway');
     }
 } 
