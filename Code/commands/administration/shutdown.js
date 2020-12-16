a6487ddc88b3e159ce838f31e72d82e6a390a869
jojo2357 <66704796+jojo2357@users.noreply.github.com>
2020-12-16 15:25:10 -0800

    Fixed shutdown not removing host status properly

--- Code/commands/administration/shutdown.js
+++ Code/commands/administration/shutdown.js
@@ -1,5 +1,5 @@
 const { platform } = require('os');
-const { killHostStatus } = require('../../util/remoteConsole.js');
+const remoteConsole = require('../../util/remoteConsole.js');
 
 module.exports = {
     commands: ['shutdown'],
@@ -7,9 +7,10 @@ module.exports = {
     restrictedToUsers: ['524411594009083933'],
     callback: async (message, arguments) => {
         await message.channel.send('oh no!');
-        if (!arguments[0] || platform().toString().toLowerCase().includes(arguments[0]))
-            killHostStatus().then(process.exit(0));
-        else
+        if (!arguments[0] || platform().toString().toLowerCase().includes(arguments[0])){
+            await remoteConsole.killHostStatus();
+            process.exit(0);
+        } else
             message.channel.send("I am " + platform().toString() + "");
     }
 } 
